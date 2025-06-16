9955b045ea6617333f12895c331b0fd4b3c1127a
YRrami <johnjohn444465@gmail.com>
2025-06-16 22:27:33 +0300

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    historyApiFallback: true, // Ensure all navigation routes fallback to index.html for client-side routing
-  },
   build: {
-    outDir: 'dist', // Directory where the build will be output
+    outDir: 'dist',
+    assetsDir: 'assets',
   },
-  base: '/', // Make sure this is '/' if you're deploying to the root path
+  server: {
+    historyApiFallback: true, // Ensures routing works for React Router
+  },
 });
